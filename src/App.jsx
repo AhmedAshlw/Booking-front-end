99116c4d46ce93ee7f3db6d62139851d427e0ddb
Mueen <hejiy2002@gmail.com>
2024-08-17 13:43:40 +0300

    creating template

---
+++ src/App.jsx
@@ -0,0 +1,51 @@
+// App.jsx
+import SigninForm from './components/SigninForm/SigninForm'
+// src/App.jsx
+// src/App.jsx
+
+
+import { useState} from 'react';
+import { Routes, Route} from 'react-router-dom';
+import NavBar from './components/NavBar/NavBar';
+import Landing from './components/Landing/Landing';
+import Dashboard from './components/Dashboard/Dashboard';
+import SignupForm from './components/SignupForm/SignupForm'
+
+
+//Services
+import * as authService from '../src/services/authService'; 
+
+
+const App = () => {
+  
+const [user, setUser] = useState(authService.getUser());
+
+const handleSignout = () => {
+ authService.signout()
+ setUser(null)
+}
+
+  return (
+    <>
+       <NavBar user={user} handleSignout={handleSignout} /> 
+      
+
+<Routes>
+  {user ? (
+    // Protected Routes:
+    <>
+      <Route path="/" element={<Dashboard user={user} />} />
+    
+    </>
+  ) : (
+    // Public Route:
+    <Route path="/" element={<Landing />} />
+  )}
+  <Route path="/signup" element={<SignupForm setUser={setUser} />} />
+  <Route path="/signin" element={<SigninForm setUser={setUser} />} />
+</Routes>
+    </>
+  )
+};
+
+export default App;
