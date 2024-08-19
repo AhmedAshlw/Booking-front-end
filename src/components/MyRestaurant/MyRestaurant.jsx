f716ab5c728d0c4f2890c6f775d57702ae88cdae
Mueen <hejiy2002@gmail.com>
2024-08-19 21:13:26 +0300

    creating componetn to show user owned restaurant

--- src/components/MyRestaurant/MyRestaurant.jsx
+++ src/components/MyRestaurant/MyRestaurant.jsx
@@ -8,7 +8,7 @@ return (
         <ul>
             {myrestaurant.map((res) => (
                 <>
-                    <Link to={`/restaurants/${res._id}`}>
+                    <Link to={`/MyRestaurants/${res._id}`}>
                         <div class="card" style={{ width: "18rem" }}>
                             <img src="..." class="card-img-top" alt="..." />
                             <div class="card-body">
