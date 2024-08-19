73c84f7e1c24cf80bfd810c84a74e11bbb839465
mj9111 <1191.1mj91.1@gmail.com>
2024-08-19 16:46:14 +0300

    forms and css

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -2,10 +2,14 @@
 
 const Dashboard = ({ user }) => {
     return (
-      <main>
+     <div>
+     <main>
         { <h1>Welcome, {user.username}</h1> }
-       
+        <video autoPlay muted loop id="myVideo">
+      <source src="https://videos.pexels.com/video-files/3769033/3769033-hd_1920_1080_25fps.mp4" type="video/mp4" />
+    </video> 
       </main>
+      </div>
     );
   };
   
--- src/components/MyRestaurant/MyRestaurant.jsx
+++ src/components/MyRestaurant/MyRestaurant.jsx
@@ -2,15 +2,27 @@ import { Link } from 'react-router-dom';
 const MyRestaurants = ({myrestaurant})=>{
 
 
-return (<>all owned restaurant
-<ul>
-{myrestaurant.map((res)=>(<><Link to={`/restaurants/${res._id}`}>
-              <h1>{res.name}</h1>
-              <p>{res.category}</p>
-              </Link></>
-))}
-</ul>
-</>)
+return (
+    <>
+        all owned restaurant
+        <ul>
+            {myrestaurant.map((res) => (
+                <>
+                    <Link to={`/restaurants/${res._id}`}>
+                        <div class="card" style={{ width: "18rem" }}>
+                            <img src="..." class="card-img-top" alt="..." />
+                            <div class="card-body">
+                                <h5 class="card-title">{res.name}</h5>
+                                <p class="card-text">{res.category}</p>
+                               
+                            </div>
+                        </div>
+                    </Link>
+                </>
+            ))}
+        </ul>
+    </>
+);
 
 
 
