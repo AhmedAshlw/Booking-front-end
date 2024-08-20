3bd9ab81bba5f388120147990d3c8caa3e643fc6
AhmedAshlw <ahmedit5216@gmail.com>
2024-08-20 12:18:19 +0300

    add styling for Forms

--- src/App.jsx
+++ src/App.jsx
@@ -12,30 +12,24 @@ import MyRestaurants from "./components/MyRestaurant/MyRestaurant";
 import RestaurantDetails from "./components/restaurant/restaurantDetails";
 import MyResDetails from "./components/MyRestaurant/MyResDeatails";
 import BookingList from "./components/Booking/BookingList";
+import MyResBookings from "./components/MyRestaurant/MyResBookings";
 //Services
 import * as authService from "../src/services/authService";
 import * as resService from "./services/restaurant";
 import * as bookingService from "./services/bookingService";
-
-import "./App.css";
-
 const App = () => {
   const [user, setUser] = useState(authService.getUser());
   const [restaurants, setRestaurant] = useState([]);
   const [Bookings, setBookings] = useState([]);
   const [myrestaurant, setmyrestaurant] = useState([]);
-
   const handleSignout = () => {
     authService.signout();
     setUser(null);
   };
-
   const navigate = useNavigate();
-
   useEffect(() => {
     const fetchAllres = async () => {
       const resData = await resService.index();
-
       setRestaurant(resData);
       if (user.isRestaurant) {
         setmyrestaurant(
@@ -49,77 +43,72 @@ const App = () => {
     };
     if (user) fetchAllres();
   }, [user]);
-
   useEffect(() => {
     const fetchAllbooks = async () => {
       const bookData = await bookingService.index();
-
       setBookings(bookData);
     };
     if (user) fetchAllbooks();
   }, [user]);
-
   const handleAddRestaurant = async (restrData) => {
     const newRestaurant = await resService.create(restrData);
     setRestaurant([...restaurants, newRestaurant]);
     navigate("/restaurants");
   };
-
   const handleAddBooking = async (bookingData, resId) => {
     const newBooking = await bookingService.create(bookingData, resId);
     setBookings([...Bookings, newBooking]);
     navigate("/restaurants");
   };
-
   return (
     <>
-      <main className="mainCont">
-        <NavBar user={user} handleSignout={handleSignout} />
-
-        <Routes>
-          {user ? (
-            // Protected Routes:
-            <>
-              <Route path="/" element={<Dashboard user={user} />} />
-              <Route
-                path="/restaurants"
-                element={<Restaurant restaurants={restaurants} />}
-              />
-              <Route
-                path="/addRestaurant"
-                element={<ResForm handleAddRestaurant={handleAddRestaurant} />}
-              />
-              <Route
-                path="/MyRestaurants"
-                element={<MyRestaurants myrestaurant={myrestaurant} />}
-              />
-              <Route
-                path="/booking"
-                element={<BookingList Bookings={Bookings} />}
-              />
-
-              {/* show my restauarnt details */}
-              <Route
-                path="/MyRestaurants/:resId"
-                element={<MyResDetails myrestaurant={myrestaurant} />}
-              />
-              <Route
-                path="/restaurants/:resId"
-                element={
-                  <RestaurantDetails handleAddBooking={handleAddBooking} />
-                }
-              />
-            </>
-          ) : (
-            // Public Route:
-            <Route path="/" element={<Landing />} />
-          )}
-          <Route path="/signup" element={<SignupForm setUser={setUser} />} />
-          <Route path="/signin" element={<SigninForm setUser={setUser} />} />
-        </Routes>
-      </main>
+      <NavBar user={user} handleSignout={handleSignout} />
+      <Routes>
+        {user ? (
+          // Protected Routes:
+          <>
+            <Route path="/" element={<Dashboard user={user} />} />
+            <Route
+              path="/restaurants"
+              element={<Restaurant restaurants={restaurants} />}
+            />
+            <Route
+              path="/addRestaurant"
+              element={<ResForm handleAddRestaurant={handleAddRestaurant} />}
+            />
+            <Route
+              path="/MyRestaurants"
+              element={<MyRestaurants myrestaurant={myrestaurant} />}
+            />
+            <Route
+              path="/booking"
+              element={<BookingList Bookings={Bookings} />}
+            />
+            {/* show my restauarnt details */}
+            <Route
+              path="/MyRestaurants/:resId"
+              element={<MyResDetails myrestaurant={myrestaurant} />}
+            />
+            {/* show my restauarnt Bookings*/}
+            <Route
+              path="/restaurants/:resId/Booking"
+              element={<MyResBookings />}
+            />
+            <Route
+              path="/restaurants/:resId"
+              element={
+                <RestaurantDetails handleAddBooking={handleAddBooking} />
+              }
+            />
+          </>
+        ) : (
+          // Public Route:
+          <Route path="/" element={<Landing />} />
+        )}
+        <Route path="/signup" element={<SignupForm setUser={setUser} />} />
+        <Route path="/signin" element={<SigninForm setUser={setUser} />} />
+      </Routes>
     </>
   );
 };
-
 export default App;
